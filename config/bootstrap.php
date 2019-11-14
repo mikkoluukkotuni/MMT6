0044a96b6fcd44f0cfeee1361b3dfe60636f77f7
mikkoluukkotuni <mikko.luukko@tuni.fi>
2019-11-14 10:53:32 +0200

    date format changes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -97,7 +97,7 @@ mb_internal_encoding(Configure::read('App.encoding'));
  * Set the default locale. This controls how dates, number and currency is
  * formatted and sets the default language to use for translations.
  */
-ini_set('intl.default_locale', 'fi_FI');
+ini_set('intl.default_locale', 'en_US');
 
 /**
  * Register application error and exception handlers.
@@ -208,11 +208,3 @@ Configure::write('Session', [
     'defaults' => 'php',
     'timeout' => 30
 ]);
-
-// Cake\I18n\Date::setToStringFormat('dd.MM.YYYY');
-// Cake\I18n\FrozenDate::setToStringFormat('dd.MM.YYYY');
-
-// \Cake\Database\Type::build('date')
-//     ->useImmutable()
-//     ->useLocaleParser()
-//     ->setLocaleFormat('dd.MM.YYYY');
