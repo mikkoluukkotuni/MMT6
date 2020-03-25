1c37c14512abe54087645347e499ce4104666c81
mikkoluukkotuni <mikko.luukko@tuni.fi>
2020-03-25 15:51:30 +0200

    Fix errors in sql code

--- sql/gitTable_add.sql
+++ sql/gitTable_add.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS `git`;
-CREATE TABLE IF NOT EXISTS `git` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_id` int(11) NOT NULL,
-  `repository` varchar(100) NOT NULL,
-  `owner` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS git (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  project_id int(11) NOT NULL,
+  repository varchar(100) NOT NULL,
+  owner varchar(40) NOT NULL,
+  PRIMARY KEY (id));
